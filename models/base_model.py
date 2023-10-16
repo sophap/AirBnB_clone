60226566e276ae8544c44c45ac4bb88344505fc0
oladayo <adekoyadayo2010@gmail.com>
2023-10-16 03:25:32 -0700

    adjust_basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@
 
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
@@ -31,7 +30,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         '''representation of the object as a string'''
@@ -43,7 +41,6 @@ class BaseModel:
         '''saves the object'''
 
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         '''shows all object attributes in dictionary format'''
---
+++ models/main_0.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from base_model import BaseModel
+
+boy = BaseModel()
+print(boy.id)
